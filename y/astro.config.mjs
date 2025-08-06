7620ce909f25ad589471038e78117bb572829742
Liacar1 <fabianquispe1290@gmail.com>
2025-08-06 19:43:23 -0400

    pollos lcoos

--- y/astro.config.mjs
+++ y/astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Liacar1.github.io',
+  base: '/Retablos',
+  outDir: './dist',
+  build: {
+    assets: 'assets'
+  }
+});
