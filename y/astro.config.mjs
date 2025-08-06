f9ce2e88d493cf7bd4e0032fbd048f100d34e9a8
Liacar1 <fabianquispe1290@gmail.com>
2025-08-06 19:52:32 -0400

    holabola

--- y/astro.config.mjs
+++ y/astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Liacar1.github.io',
-  base: '/Retablos',
+  base: '/y',
   outDir: './dist',
   build: {
     assets: 'assets'
