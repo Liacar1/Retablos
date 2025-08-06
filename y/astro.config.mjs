7680d8afa1f0a7a7ee9b059052ea14ef73429310
Huanca Carvajal Alain Alexandro <huancaalain24@gmail.com>
2025-08-06 12:51:18 -0400

    version fucnional creo

--- y/astro.config.mjs
+++ y/astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  // Si vas a subir a GitHub Pages, usa el nombre de tu repositorio
+  // Si es para desarrollo local, comenta esta línea
+  // base: '/nombre-de-tu-repositorio/',
+});
