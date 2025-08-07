20d9b8189c91b24cba3ee8edd6ba0e63d297377f
Liacar1 <fabianquispe1290@gmail.com>
2025-08-07 14:39:32 -0400

    correxiond ela navegacion

--- y/astro.config.mjs
+++ y/astro.config.mjs
@@ -1,10 +1,10 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  site: 'https://liacar1.github.io/Retablos',
+  site: 'https://Liacar1.github.io/Retablos/',
   base: '/Retablos/',
   outDir: './dist',
-  build: {
-    assets: 'assets'
-  }
 });
